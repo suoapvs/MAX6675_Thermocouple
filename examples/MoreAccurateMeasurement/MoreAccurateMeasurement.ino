88cb72f924476d55bf65bf9b1aee74f07caf218f
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2018-04-15 20:45:13 +0300

    Added a new example.

---
+++ examples/MoreAccurateMeasurement/MoreAccurateMeasurement.ino
@@ -0,0 +1,53 @@
+/*
+  Thermocouple.
+
+  Reads a temperature from a thermocouple based
+  on the MAX6675 driver and displays it in the default Serial.
+
+  https://github.com/YuriiSalimov/MAX6675_Thermocouple
+
+  Created by Yurii Salimov, April, 2018.
+  Released into the public domain.
+*/
+#include <MAX6675_Thermocouple.h>
+
+#define SCK_PIN 3
+#define CS_PIN  4
+#define SO_PIN  5
+
+/**
+   How many readings are taken to determine a mean temperature.
+   The more values, the longer a calibration is performed,
+   but the readings will be more accurate.
+*/
+#define READINGS_NUMBER  10
+
+/**
+   Delay time between a temperature readings
+   from the temperature sensor (ms).
+*/
+#define DELAY_TIME  100
+
+MAX6675_Thermocouple* thermocouple = NULL;
+
+void setup() {
+  Serial.begin(9600);
+  thermocouple = new MAX6675_Thermocouple(
+    SCK_PIN, CS_PIN, SO_PIN,
+    READINGS_NUMBER, DELAY_TIME
+  );
+  /* or
+    thermocouple = new MAX6675_Thermocouple(SCK_PIN, CS_PIN, SO_PIN);
+    thermocouple->setReadingsNumber(READINGS_NUMBER);
+    thermocouple->setDelayTime(DELAY_TIME);
+  */
+}
+
+void loop() {
+  Serial.print("Temperature: ");
+  Serial.print(String(thermocouple->readCelsius()) + " C, ");
+  Serial.print(String(thermocouple->readFahrenheit()) + " F, ");
+  Serial.println(String(thermocouple->readKelvin()) + " K");
+  delay(1000);
+}
+
